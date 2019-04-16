e74c2af06a64d6c887a143067de9f81d65274119
ldoppertin <lea.doppertin@googlemail.com>
2019-04-16 17:20:15 +0200

    edited the javascript file main and deleted the unused scripts file

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/rabbit.jpg') {
+      myImage.setAttribute ('src','images/perspective.jpg');
+    } else {
+      myImage.setAttribute ('src','rabbit.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Welcome to our Website, ' + myName + '!';
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to our Website, ' + storedName + '!';
+}  
+
+myButton.onclick = function() {
+    setUserName();
+  }
