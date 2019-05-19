6320eebaa5b1223fd0b4b437af365fc6ee24be6b
vivianeschm <viviane.schmidt2@gmail.com>
2019-05-19 20:14:06 +0200

    Adding experiment description and shuffle function

---
+++ HW3/departure-point-master/03_trials.js
@@ -0,0 +1,7 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+    key_press_practice : get_trials(0,12),
+    key_press_experiment: get_trials(12,61)
+};
